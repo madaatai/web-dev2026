d7a13f4cb133e4ec2a1c95c7121714f150d6269c
madina <madaataibek@gmail.com>
2026-02-20 16:34:23 +0500

    Add files via upload

---
+++ lab3/JavaScriptInfo/javascriptFundamentals/rewriteifelse.js
@@ -0,0 +1,14 @@
+let message;
+
+if (login == 'Employee') {
+  message = 'Hello';
+} else if (login == 'Director') {
+  message = 'Greetings';
+} else if (login == '') {
+  message = 'No login';
+} else {
+  message = '';
+}
+
+
+let mesage = (login == 'Employee') ? 'Hello' : (login == 'Director') ? 'Greetings' : (login == '') ? 'No login' : '';
